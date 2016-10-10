109087c0cedd40cca2195e84ef2dd34244ad1b39
Isaac True <isaac.true@viaboxx.de>
2016-10-10 16:07:10 +0200

    Added Debian support

--- manifests/params.pp
+++ manifests/params.pp
@@ -24,6 +24,24 @@ class pureftpd::params {
       $pgsql_conf_erb  = 'pure-ftpd.conf.erb'
       $pgsql_conf_path = "${config_dir}/pureftpd-pgsql.conf"
     }
+    'Debian': {
+      $package_name         = 'pure-ftpd'
+
+      $config_dir      = '/etc/pure-ftpd'
+      $service_name    = 'pure-ftpd'
+
+      $conf_erb        = 'pure-ftpd.conf.erb'
+      $conf_path       = "${config_dir}/pure-ftpd.conf"
+
+      $ldap_conf_erb   = 'pure-ftpd.conf.erb'
+      $ldap_conf_path  = "${config_dir}/pureftpd-ldap.conf"
+
+      $mysql_conf_erb  = 'pure-ftpd.conf.erb'
+      $mysql_conf_path = "${config_dir}/pureftpd-mysql.conf"
+
+      $pgsql_conf_erb  = 'pure-ftpd.conf.erb'
+      $pgsql_conf_path = "${config_dir}/pureftpd-pgsql.conf"
+    }
     default:{
       fail("Module ${module_name} is not supported on ${::operatingsystem}")
     }
